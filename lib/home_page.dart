0d16e5dc98b9802a1fb7a6b0060bfaf413856aa5
hyunsik jung <ball1241@naver.com>
2023-07-11 17:48:36 +0900

    두번쨰 코드

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import 'second.dart';
 import 'feed.dart';
 
 class HomePage extends StatelessWidget {
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import 'second.dart';
 import 'home_page.dart';
 
 void main() {
@@ -12,6 +12,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final List<String> images = [
+      "https://i.namu.wiki/i/ZK5KQRHUtu4xDpDJkYGimSK6rzlG1H-LNGeMb9se6XWZcmx4GXSMvwKe946EaCXzIwX2wCibID24I1WKTgwPkeVfBP_5U2DCfIWl1tX_Xq8hXdNYY1yafieuSyZiS9EW3S_0lzcBTDTH41scRtysaA.webp"
+    ];
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: HomePage(),
