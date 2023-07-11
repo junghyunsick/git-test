b3b79c30e8da80cce9d75b419224e9ab0b006758
hyunsik jung <ball1241@naver.com>
2023-07-11 17:09:21 +0900

    처음코딩

---
+++ lib/feed.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Feed extends StatefulWidget {
+  const Feed({
+    Key? key,
+    required this.imageUrl,
+    required this.product,
+    required this.sport,
+    required this.mbti,
+  }) : super(key: key);
+
+  final String imageUrl;
+  final String product;
+  final String sport;
+  final String mbti;
+  // 이미지를 담을 변수
+
+  @override
+  State<Feed> createState() => _FeedState();
+}
+
+class _FeedState extends State<Feed> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        // CilpRRect 를 통해 이미지에 곡선 border 생성
+        ClipRRect(
+          borderRadius: BorderRadius.circular(8),
+          // 이미지
+          child: Image.network(
+            widget.imageUrl, // 10번째 줄의 imageUrl 가져오기
+            width: 100,
+            height: 100,
+            fit: BoxFit.cover,
+          ),
+        ),
+        SizedBox(width: 12),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                widget.product,
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.black,
+                ),
+                softWrap: false,
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+              ),
+              SizedBox(height: 2),
+              Text(
+                widget.sport, //'취미: 농구',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.black,
+                ),
+              ),
+              SizedBox(height: 4),
+              Text(
+                widget.mbti, // 'MBTI: ISTP',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.black,
+                ),
+              ),
+              Row(
+                children: [
+                  Spacer(),
+                  GestureDetector(
+                    onTap: () {},
+                    child: Row(
+                      children: [
+                        Icon(
+                          CupertinoIcons.plus,
+                          color: Colors.black,
+                          size: 16,
+                        ),
+                        Text(
+                          '더보기',
+                          style: TextStyle(color: Colors.black),
+                        ),
+                      ],
+                    ),
+                  )
+                  // 빈 칸
+                  // 하트 아이콘
+                  // '1'
+                ],
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/home_page.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'feed.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final List<String> images = [
+      "https://ifh.cc/g/kDaKF7.jpg",
+      "https://ifh.cc/g/tVyHZn.jpg",
+      "https://ifh.cc/g/LsrBHx.jpg",
+      "https://ifh.cc/g/28FsqF.jpg",
+      "https://ifh.cc/g/1xXkcs.jpg",
+    ];
+    final List<String> productNames = [
+      "이름: 정현식",
+      "이름:정선호",
+      "이름:권경운",
+      "이름:장재용",
+      "이름:한상철",
+    ];
+    final List<String> Sports = [
+      "취미: 농구하기",
+      "취미: 노래부르기",
+      "취미: 노래방, 헬스",
+      "취미: 헬스, 요리",
+      "취미: 게임, 음주",
+    ];
+    final List<String> Mbtis = [
+      "MBTI: ISTP",
+      "MBTI: ISFP",
+      "MBTI: ISFP",
+      "MBTI: ISFP",
+      "MBTI: ISTP",
+    ];
+    return Scaffold(
+      appBar: AppBar(
+        leading: Row(
+          children: [
+            SizedBox(width: 16),
+            Text(
+              '십일조',
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+            Icon(
+              Icons.keyboard_arrow_down_rounded,
+              color: Colors.black,
+            ),
+          ],
+        ),
+        leadingWidth: 100,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: ListView.separated(
+          itemCount: images.length, // 이미지 개수만큼 보여주기
+          itemBuilder: (context, index) {
+            final image = images[index];
+            final productName = productNames[index];
+            final Sport = Sports[index];
+            final Mbti = Mbtis[index]; // index에 해당하는 이미지
+            return Padding(
+              padding: const EdgeInsets.symmetric(vertical: 12),
+              child: Feed(
+                  imageUrl: image,
+                  product: productName,
+                  sport: Sport,
+                  mbti: Mbti), // imageUrl 전달
+            );
+          },
+          separatorBuilder: (context, index) {
+            return Divider();
+          },
+        ),
+      ),
+    );
+  }
+}
